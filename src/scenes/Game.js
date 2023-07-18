8ba8940419cc6c0c733de2c0e94cadf3043086a0
Ian Jones <ianjones834@protonmail.com>
2023-07-18 16:31:26 -0400

    added carrots

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,11 +1,19 @@
 import Phaser from "../lib/phaser.js";
 
+import Carrot from "../game/Carrot.js";
+
 export default class Game extends Phaser.Scene {
   /** @type {Phaser.Physics.Arcade.Sprite} */
   player;
 
   /** @type {Phaser.Physics.Arcade.StaticGroup} */
   platforms;
+
+  /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+  cursors;
+
+  /** @type {Phaser.Physics.Arcade.Group} */
+  carrots;
 
   constructor() {
     super('game');
@@ -15,10 +23,14 @@ export default class Game extends Phaser.Scene {
     this.load.image('background', 'assets/bg_layer1.png');
     this.load.image('platform', 'assets/ground_grass.png');
     this.load.image('bunny-stand', 'assets/bunny1_stand.png');
+    this.load.image('carrot', 'assets/carrot.png');
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
   }
 
   create() {
-    this.add.image(240, 320, 'background');
+    this.add.image(240, 320, 'background').setScrollFactor(1, 0);
 
     this.platforms = this.physics.add.staticGroup();
 
@@ -47,6 +59,21 @@ export default class Game extends Phaser.Scene {
     this.player.body.checkCollision.right = false;
 
     this.cameras.main.startFollow(this.player);
+    this.cameras.main.setDeadzone(this.scale.width * 1.5);
+
+    this.carrots = this.physics.add.group({
+      classType: Carrot
+    })
+
+    this.physics.add.collider(this.platforms, this.carrots);
+
+    this.physics.add.overlap(
+      this.player,
+      this.carrots,
+      this.handleCollectCarrot,
+      undefined,
+      this
+    )
   }
 
   update() {
@@ -56,8 +83,9 @@ export default class Game extends Phaser.Scene {
 
       const scrollY = this.cameras.main.scrollY;
       if (platform.y >= scrollY + 700) {
-        platform.y = scrollY - Phaser.Math.Between(50, 150);
+        platform.y = scrollY - Phaser.Math.Between(50, 100);
         platform.body.updateFromGameObject()
+        this.addCarrotAbove(platform);
       }
     })
 
@@ -66,5 +94,49 @@ export default class Game extends Phaser.Scene {
     if (touchingDown) {
       this.player.setVelocityY(-300);
     }
+
+    if (this.cursors.left.isDown && !touchingDown) {
+      this.player.setVelocityX(-200);
+    }
+    else if (this.cursors.right.isDown && !touchingDown) {
+      this.player.setVelocityX(200);
+    }
+    else {
+      this.player.setVelocityX(0);
+    }
+
+    this.horizontalWrap(this.player);
+  }
+
+  /**
+   *
+   * @param {Phaser.GameObjects.Sprite} sprite
+   */
+  horizontalWrap(sprite) {
+    const halfWidth = sprite.displayWidth * 0.5;
+    const gameWidth = this.scale.width;
+
+    if (sprite.x < -halfWidth) {
+      sprite.x = gameWidth + halfWidth;
+    }
+    else if (sprite.x > gameWidth + halfWidth) {
+      sprite.x = -halfWidth;
+    }
+  }
+
+  /**
+   * @param {Phaser.GameObjects.Sprite} sprite
+   */
+
+  addCarrotAbove(sprite) {
+    const y = sprite.y - sprite.displayHeight;
+
+    /** @type {Phaser.Physics.Arcade.Sprite} */
+    const carrot = this.carrots.get(sprite.x, y, 'carrot');
+    this.add.existing(carrot);
+
+    carrot.body.setSize(carrot.width, carrot.height);
+
+    return carrot;
   }
 }
